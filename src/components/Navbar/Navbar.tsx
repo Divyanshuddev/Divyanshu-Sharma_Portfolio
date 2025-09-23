604c80b308dcbd95d942b38420241ce4a4759d07
Divyanshuddev <divyanshu311098@gmail.com>
2025-09-23 15:46:33 +0530

    Change the resume link

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -72,7 +72,7 @@ const Navbar = () => {
                 whileInView={{ scale: 1.1 }}
                 style={{ backgroundColor: "#0A192F", border: "none" }}
               >
-                <Button variant="contained" sx={styles.resumeButton} size="large" href="https://drive.google.com/file/d/1eg28aDIOrdImYW9AqftADqe8gBCan9zQ/view?usp=sharing" target="_blank">Resume</Button>
+                <Button variant="contained" sx={styles.resumeButton} size="large" href="https://drive.google.com/file/d/1GxHLWH5bOexy60VV50z5LU4PmLd1Ba8p/view?usp=sharing" target="_blank">Resume</Button>
               </motion.button>
             </motion.div>
           </Stack>
